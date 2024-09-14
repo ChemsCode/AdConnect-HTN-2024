80524198eae1e907a7a698b1fbdf0597de959378
st2-ev <steve.s.paul22@gmail.com>
2024-09-14 13:48:43 -0400

    added routing and shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomeView from "./pages/HomeView";
+import LandingView from "./pages/LandingView";
+import ChatView from "./pages/ChatView";
+import ProfileView from "./pages/ProfileView";
+
+
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingView />,
+  },
+  {
+    path: "/home",
+    element: <HomeView />,
+  },
+  {
+    path: "/chat",
+    element: <ChatView />,
+  },
+  {
+    path: "/profile",
+    element: <ProfileView />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConvexProvider client={convex}>
-      <App />
+    <RouterProvider router={router} />
     </ConvexProvider>
   </React.StrictMode>,
 );
---
+++ src/pages/ProfileView.tsx
@@ -0,0 +1,8 @@
+export default function ProfileView() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <div>Welcome to the profile page</div>
+        </div>
+    );
+}
